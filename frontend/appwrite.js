f1645199a945b9a2126b0a0435917bcdf3df43b7
CHEGEBB <chegephil24@gmail.com>
2024-10-12 10:58:17 +0300

    Refactor appwrite.js to add new storage ID

--- frontend/appwrite.js
+++ frontend/appwrite.js
@@ -1,5 +1,8 @@
+import { Client } from 'react-native-appwrite';
 
-export const appwriteConfig ={
+
+
+export const Config ={
     endpoint :"https://cloud.appwrite.io/v1",
     platform : "com.health-master.healthmaster",
     projectId : "6704d37c003c8a2f6a36",
@@ -8,4 +11,23 @@ export const appwriteConfig ={
     appoinmentsCollectionId: "670a254100339e546aa4",
     medicationCollectionId: "670a256a00336a73c6d3",
     remindersCollectionId : "670a259c000d92ef0f0a",
+    storageId : "670a2a300017fdaee701"
 }
+// Init your  Native SDK
+const client = new Client();
+
+client
+    .setEndpoint(Config.endpoint) 
+    .setProject(Config.projectId)
+    .setPlatform(Config.platform) 
+;
+
+const account = new Account(client);
+
+// Register User
+account.create(ID.unique(), 'me@example.com', 'password', 'Jane Doe')
+    .then(function (response) {
+        console.log(response);
+    }, function (error) {
+        console.log(error);
+    });
