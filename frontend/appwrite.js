e95392966d8be64af987b4cf3cfd9eb5ff96ecab
CHEGEBB <chegephil24@gmail.com>
2024-10-12 10:36:00 +0300

    Refactor appwrite.js to add new collection IDs

--- frontend/appwrite.js
+++ frontend/appwrite.js
@@ -5,4 +5,7 @@ export const appwriteConfig ={
     projectId : "6704d37c003c8a2f6a36",
     databaseId : "670a2468000caae299eb",
     userCollectionId : "670a248800049761218e",
+    appoinmentsCollectionId: "670a254100339e546aa4",
+    medicationCollectionId: "670a256a00336a73c6d3",
+    remindersCollectionId : "670a259c000d92ef0f0a",
 }
