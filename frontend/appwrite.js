e777da59d2b5f79ec3a33488a3a2aba51920d84a
CHEGEBB <chegephil24@gmail.com>
2024-10-12 10:28:16 +0300

    Refactor appwrite.js to add databaseId configuration

--- frontend/appwrite.js
+++ frontend/appwrite.js
@@ -3,4 +3,6 @@ export const appwriteConfig ={
     endpoint :"https://cloud.appwrite.io/v1",
     platform : "com.health-master.healthmaster",
     projectId : "6704d37c003c8a2f6a36",
+    databaseId : "670a2468000caae299eb",
+    userCollectionId : "670a248800049761218e",
 }
