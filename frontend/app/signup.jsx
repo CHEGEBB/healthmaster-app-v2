894a0bdb295c5c010d1df2bec67e2b94a9ba3e4e
CHEGEBB <chegephil24@gmail.com>
2024-10-17 19:59:36 +0300

    Refactor font import in signup.jsx to use "Rubik-Bold" instead of "Kanit-Bold"

--- frontend/app/signup.jsx
+++ frontend/app/signup.jsx
@@ -440,7 +440,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: '#fff',
-    fontFamily: "Kanit-Bold",
+    fontFamily: "Rubik-Bold",
   },
   signupTextContainer: {
     alignItems: 'flex-start',
